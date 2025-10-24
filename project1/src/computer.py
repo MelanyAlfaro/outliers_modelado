329557d7cba27248ce69b33483072a806228d2ab
Randall Eduardo Araya Nuñez <91082488+ranAraya27@users.noreply.github.com>
2025-10-24 13:18:27 -0600

    DEV-001-RAN: Implement parent Computer class (#12)
    * feat: add initial common Makefile for project build configuration
    
    * feat: add Makefile to project1 for build configuration
    
    * feat: add README file for project1 documentation
    
    * feat: add class diagram for project1 design documentation
    
    * feat: implement abstract Computer class

---
+++ project1/src/computer.py
@@ -0,0 +1,31 @@
+from abc import ABC, abstractmethod
+from collections import deque
+
+from .message import Message
+from .event import Event
+
+
+class Computer(ABC):
+    def __init__(self, ID: int) -> None:
+        self.ID: int = ID
+        self.end_time: float = 0.0
+        self.busy: bool = False
+        self.busy_time: float = 0.0
+        self.message_queue: deque[Message] = deque()
+
+    def enqueue_message(self, message: Message) -> None:
+        self.message_queue.append(message)
+
+    def get_enqueued_messages(self) -> int:
+        return len(self.message_queue)
+
+    def get_state(self) -> str:
+        return "busy" if self.busy else "free"
+
+    @abstractmethod
+    def process_message(self, now: float) -> Event:
+        raise NotImplementedError
+
+    @abstractmethod
+    def generate_processing_time(self, message: Message) -> float:
+        raise NotImplementedError
