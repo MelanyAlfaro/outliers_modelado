80b6308050d067fb2246bb413aaf7d0d125abacc
Evan Chen <evan.chen@ucr.ac.cr>
2025-10-30 00:07:45 -0600

    feat: implement store message method

--- project1/src/stats_collector.py
+++ project1/src/stats_collector.py
@@ -1,14 +1,26 @@
+from collections import defaultdict
+
+
 from message import Message
-from computer import Computer
+from computer import Computer, MASTER_COMPUTER, WORKER_COMPUTER, LAZY_COMPUTER
+from message_type import MessageType
 
 
 class StatsCollector:
     def __init__(self) -> None:
-        self.messages_dict: dict[str, list[Message]] = dict()
+        self.messages_dict: dict[str, list[Message]] = defaultdict(list)
         self.joint_work_time: float = 0.0
 
     def store_msg(self, message: Message) -> None:
-        pass
+        if message.rejected:
+            message_type = MessageType.REJECTED_MSG
+        elif message.source == WORKER_COMPUTER:
+            message_type = MessageType.SENT_MSG_FROM_WORKER
+        elif message.source == LAZY_COMPUTER:
+            message_type = MessageType.SENT_MSG_FROM_LAZY
+        else:
+            raise AssertionError("Message does not fit with types")
+        self.messages_dict[message_type].append(message)
 
     def get_msgs_avg_in_sys_time(self) -> tuple[float]:
         pass
